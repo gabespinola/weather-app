ce7a495c1152749bfbcd750b508cbaf666d57c9c
Gabriel Espínola <gabrielsespinola@gmail.com>
2026-03-11 10:53:31 -0300

    finalizando javascript

--- script.js
+++ script.js
@@ -14,11 +14,55 @@ document.querySelector('#search').addEventListener('submit',async(event)=>{
     const results=await fetch(apiurl);
     const json =  await results.json()
 
-    
+    if(json.cod=== 200){
+        showInfo({
+            city: json.name,
+            country: json.sys.country,
+            temp: json.main.temp,
+            tempmax: json.main.temp_max,
+            tempmin: json.main.temp_min,
+            description: json.weather[0].description,
+            tempicon: json.weather[0].icon,
+            windspeed: json.wind.speed,
+            humidity: json.main.humidity,
+        })
+
+
+    }else{
+        showalert('Não foi possivel localizar!')
+
+    }
 });
 
 
+function showInfo(json){
+   showalert('') 
 
+   document.querySelector("#weather").classList.add('show');
+
+   document.querySelector("#title").innerHTML= `${json.city}, ${json.country}`;
+  
+   document.querySelector("#temp_value").innerHTML= `${json.temp.toFixed(1).toString().replace('.',',')} <sup>°C</sup>`
+
+   document.querySelector("#temp_description").innerHTML= `${json.description}`
+
+   document.querySelector("#temp_img").setAttribute('src',` https://openweathermap.org/img/wn/${json.tempicon}@2x.png`)
+   
+   document.querySelector("#temp_max").innerHTML= `${json.tempmax.toFixed(1).toString().replace('.',',')} <sup>°C</sup>`
+
+   document.querySelector("#temp_min").innerHTML= `${json.tempmin.toFixed(1).toString().replace('.',',')} <sup>°C</sup>`
+
+   document.querySelector("#humidity").innerHTML= `${json.humidity}%`
+
+   document.querySelector("#wind").innerHTML= `${json.windspeed.toFixed(1)}km/h`
+   
+
+
+
+
+
+
+}
 
 
 function showalert(msg){
